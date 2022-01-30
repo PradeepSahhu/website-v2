b793d63d8bb066e73d6fdbbe33b07d88a3a7c32c
Pradeep Sahu <specialreaderman.com@gmail.com>
2022-01-30 18:11:58 +0530

    update version

--- js/app.js
+++ js/app.js
@@ -1,47 +1,11 @@
 
 
 
-// alert("is it working?");
-// const Scriptjs = ("https://cdn.jsdelivr.net/npm/typed.js@2.0.12", function(){
-//
-// })
-// var scriptjs = require('Scriptjs');
-//   var typed = new Typed('.typing', {
-//     strings: ['I am an AI robot created by my master to serve on his behalf '],
-//     typeSpeed: 50,
-//     backSpeed: 50,
-//     loop: true
-//   });
-// src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"
-
-// var audio = new Audio('C:\Users\speci\Desktop\web\typing.wav');
-// audio.play();
-
-
-
-// function showAlert(){
-//   alert("Here all the sensitive information. Unauthorized access or usage of any of the materials are strictly prohibited...");
-// }
-// document.getElementsByClassName("clicking").addEventListener("click", showAlert());
-
 // -------------------------------------------Important------------------------------------------------------------------
-// document.querySelector(".education").addEventListener("click", function(){
-//   alert("this is unauthorized area... YOu must have access to get documents here...")
-// });
-
-$(".education").click(function(){
-  alert("this is unauthorized area");
+document.querySelector(".education").addEventListener("click", function(){
+  alert("this is unauthorized area... YOu must have access to get documents here...")
 });
 
-
-// document.getElementById("myButton").onclick = function () {
-//         location.href = "E:\Vs-code\web\html\Education and Projects.html";
-//     };
-// document.querySelector(".button").addEventListener("click", function button(req,res){
-//   res.sendFiles(__dirname+"/Education and Projects.html")
-// })
-
-// $(".bodyClass").on("click", function(){
-//   var audio = new Audio("C:\Users\speci\Desktop\web\videoplayback.mp4");
-//   audio.play()
+// $(".education").click(function(){
+//   alert("this is unauthorized area");
 // });
